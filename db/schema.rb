3b5f1cbff1ac1786ffa3c0956dc4c90695976137
Zach Williams <zlw5009@gmail.com>
2016-01-01 22:30:45 -0500

    Updated star rating to include average

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160102013913) do
+ActiveRecord::Schema.define(version: 20160102024148) do
 
   create_table "places", force: :cascade do |t|
     t.string   "name"
@@ -20,10 +20,11 @@ ActiveRecord::Schema.define(version: 20160102013913) do
     t.string   "phone"
     t.string   "website"
     t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
     t.float    "latitude"
     t.float    "longitude"
+    t.integer  "total_average_rating", default: 0
   end
 
   create_table "reviews", force: :cascade do |t|
